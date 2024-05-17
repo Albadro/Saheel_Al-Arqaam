ffea91c5ecbd435323df619d18878e36c5fbd62c
albadr <kaitokitthebest@gmail.com>
2024-05-18 01:09:34 +0300

    the basic js logic (the game is functioning in the very basic form)

---
+++ script/script.js
@@ -0,0 +1,84 @@
+const letters = ["a", "b", "c", "d", "e"];
+let prevElements = [];
+let allElements = [];
+document.querySelectorAll(".sqr").forEach((element) => {
+    element.addEventListener("click", handleClick);
+    allElements.push(element.id);
+});
+let possibleNexts = allElements;
+let n = 1;
+
+// the called function when a sqr is clicked
+function handleClick(event) {
+    function getPossibleNexts(id) {
+        function pusher(num, letter, order) {
+            let jump;
+            if (order == "st") {
+                jump = 2;
+            } else {
+                jump = 1;
+            }
+            const up = num + jump;
+            const down = num - jump;
+            switch (true) {
+                case num == 3 || (num % 2 == 0 && order == "nd"):
+                    possibleNexts.push(letter + up, letter + down);
+                    break;
+                case num == 2 && order == "st":
+                    possibleNexts.push(letter + up);
+                    break;
+                case num == 4 && order == "st":
+                    possibleNexts.push(letter + down);
+                    break;
+                case num == 1:
+                    possibleNexts.push(letter + up);
+                    break;
+                case num == 5:
+                    possibleNexts.push(letter + down);
+                    break;
+            }
+        }
+        const index = letters.indexOf(id[0]);
+        const num = parseInt(id[1], 10); // you can also use the function "Number" as `const num = Number(id[1]);`
+        let stNxtLetter, ndNxtLetter, stPrvLetter, ndPrvLetter;
+        let possibleNexts = [];
+        if (index < 4) {
+            stNxtLetter = letters[index + 1];
+            pusher(num, stNxtLetter, "st");
+        }
+        if (index < 3) {
+            ndNxtLetter = letters[index + 2];
+            pusher(num, ndNxtLetter, "nd");
+        }
+        if (index > 0) {
+            stPrvLetter = letters[index - 1];
+            pusher(num, stPrvLetter, "st");
+        }
+        if (index > 1) {
+            ndPrvLetter = letters[index - 2];
+            pusher(num, ndPrvLetter, "nd");
+        }
+        const purePossibleNexts = possibleNexts.filter(
+            (element) => !prevElements.includes(element)
+        );
+        return purePossibleNexts;
+    }
+    const id = event.target.id;
+    const element = document.getElementById(id);
+    if (possibleNexts.includes(id)) {
+        element.innerHTML = n;
+        n++;
+        prevElements.push(id);
+        possibleNexts = getPossibleNexts(id);
+        if (n == 26) {
+            // win animation
+            console.log("you won!");
+        } else if (possibleNexts.length === 0) {
+            //lose animation
+            console.log("you lost!");
+        }
+    } else {
+        //refused animation
+        console.log("refused!");
+    }
+}
